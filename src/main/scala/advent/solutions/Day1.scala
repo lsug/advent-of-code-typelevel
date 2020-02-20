192fb3abf1b45e7ec1997bc3ef7a186462bdd9b8
Kyoto <kaixinang94@gmail.com>
2020-02-20 21:46:50 +0000

    #1_Day1_solution

--- src/main/scala/advent/solutions/Day1.scala
+++ src/main/scala/advent/solutions/Day1.scala
@@ -3,5 +3,9 @@ package advent.solutions
 object Day1 {
   // TODO: Write the code to solve day 1 here
 
-  def fuel(mass: Int): Int = ???
+  def fuel(mass: Int): Int = {
+    (mass / 3) - 2
+  }
+
+
 }
