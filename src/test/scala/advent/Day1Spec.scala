12ae47267d28faf64a1c696c8492d7fb250405a7
Kai X. Ang <kaixinang94@gmail.com>
2020-02-22 21:46:36 +0000

    Revert "Solved Day1 problem."
    This reverts commit a55cf53f982ae4b8a43b0e57a73afccb2780ab9f.

--- src/test/scala/advent/Day1Spec.scala
+++ src/test/scala/advent/Day1Spec.scala
@@ -19,107 +19,6 @@ final class Day1Spec extends FlatSpec with Matchers {
     143030
   )
 
-  val puzzle1Input: List[Int] = List(80891,
-    109412,
-    149508,
-    114894,
-    97527,
-    59858,
-    113548,
-    110516,
-    97454,
-    84612,
-    84578,
-    87923,
-    102675,
-    114312,
-    144158,
-    147190,
-    53051,
-    115477,
-    50870,
-    122198,
-    91019,
-    114350,
-    88592,
-    119617,
-    61012,
-    67012,
-    85425,
-    62185,
-    124628,
-    98505,
-    53320,
-    123834,
-    105862,
-    113715,
-    149328,
-    72125,
-    107301,
-    110684,
-    86037,
-    102012,
-    133227,
-    66950,
-    64761,
-    141015,
-    132134,
-    87171,
-    84142,
-    80355,
-    124967,
-    87973,
-    98062,
-    79312,
-    120108,
-    97537,
-    89584,
-    55206,
-    68135,
-    83286,
-    66726,
-    101805,
-    72996,
-    113109,
-    116248,
-    132007,
-    128378,
-    52506,
-    113628,
-    62277,
-    73720,
-    101756,
-    141675,
-    107011,
-    81118,
-    60598,
-    122703,
-    129905,
-    67786,
-    50982,
-    96193,
-    70006,
-    137087,
-    136121,
-    146902,
-    74781,
-    50569,
-    102645,
-    99426,
-    97857,
-    122801,
-    55022,
-    81433,
-    60509,
-    66906,
-    142099,
-    126652,
-    103240,
-    141014,
-    55579,
-    143169,
-    125521)
-
   "fuel" should "have a mass of 2 for a module of mass 12" in {
     Day1.fuel(12) should be(2)
   }
@@ -140,10 +39,6 @@ final class Day1Spec extends FlatSpec with Matchers {
     Day1.sumOfFuel(input) should be(3497399)
   }
 
-  it should "return answer for part 1 of the puzzle" in {
-    Day1.sumOfFuel(puzzle1Input) should be (3267638)
-  }
-
   "total fuel" should "have a mass of 2 for a module of mass 14" in {
     Day1.totalFuel(14) should be(2)
   }
@@ -158,9 +53,5 @@ final class Day1Spec extends FlatSpec with Matchers {
 
   it should "solve part 2 of the puzzle" in {
     Day1.sumOfTotalFuel(input) should be(5243207)
-  }
-
-  it should "return answer for part 2 of the puzzle" in {
-    Day1.sumOfTotalFuel(puzzle1Input) should be(4898585)
   }
 }
